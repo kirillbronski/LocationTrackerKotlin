5ceaf4d7f9d7504614139d05aca9da4a597ded43
Kirill <bronskidev@gmail.com>
2022-06-18 17:26:17 +0300

    renamed activity in common module

--- common/src/main/java/com/foxminded/android/locationtrackerkotlin/CommonActivity.kt
+++ common/src/main/java/com/foxminded/android/locationtrackerkotlin/CommonActivity.kt
@@ -11,7 +11,7 @@ abstract class CommonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_common)
     }
 
     fun displayFragment(
